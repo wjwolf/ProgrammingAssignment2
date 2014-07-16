2416b3c036c34d2a5cf17c76bfeb90ee678b5290
wjwolf <bill.wolf.iii@gmail.com>
2014-07-15 20:50:15 -0400

    Bill Wolf -- Assignment 2 solution
    Bill Wolf -- Assignment 2 solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Bill Wolf 
+## rprog-005 Assignment #2
+## 7/15/14
 
-## Write a short comment describing this function
+## answer is based on course example provided at 
+##https://class.coursera.org/rprog-005/human_grading/
+##view/courses/972576/assessments/3/submissions
 
+## This function creates a special "matrix" object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinv <- function(solve) m <<- solve
+    getinv <- function() m
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
+## This function computes the inverse of the special "matrix" returned by 
+## makeCacheMatrix above. If the inverse has already been calculated (and 
+## the matrix has not changed), then cacheSmolve retrieves the inverse 
+## from the cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getinv()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinv(m)
+    m
 }
+
+
+
+
